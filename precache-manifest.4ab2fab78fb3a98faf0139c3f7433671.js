ace9792d108cc952de8fd2746dd915a4dffa5dfb
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-06-15 17:54:00 +0800

    Updates

---
+++ precache-manifest.4ab2fab78fb3a98faf0139c3f7433671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5ba010eb416a954152ebbb5ab5d5db",
+    "url": "/basic-to-do/index.html"
+  },
+  {
+    "revision": "a9c9fea8914f908a7209",
+    "url": "/basic-to-do/static/js/2.d466adb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-to-do/static/js/2.d466adb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6df2ce3d58b2fe85bb",
+    "url": "/basic-to-do/static/js/main.8e195306.chunk.js"
+  },
+  {
+    "revision": "e2e2ff1706a9eef18dde",
+    "url": "/basic-to-do/static/js/runtime-main.c930c700.js"
+  }
+]);
